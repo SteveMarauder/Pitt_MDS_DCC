3fc2f5b9758eeb07bcfa0145952a0213650de161
SteveMarauder <stevenchunhsingwu@gmail.com>
2024-11-10 23:04:01 +0800

    upload csv files

--- W11_02.py
+++ W11_02.py
@@ -66,3 +66,51 @@ plt.show()
 
 
 # %%
+df = pd.DataFrame({'x':np.linspace(-3,3,num=15)})
+
+# %%
+df['trend'] = my_intercept + my_slope * np.sin(df.x)
+
+rg = np.random.default_rng(2100)
+df['y'] = rg.normal(loc=df.trend, scale=my_sigma, size=df.shape[0])
+
+fig, ax = plt.subplots()
+# TRUE TREND
+ax.plot(df_viz.x, df_viz.trend, color='crimson', linewidth=1.5)
+# Variation around the trend - showing 2 intervals
+# 2 sigma interval
+ax.fill_between(df_viz.x, df_viz.obs_lwr_95, df_viz.obs_upr_95, facecolor = 'crimson', alpha=0.35)
+# 1 sigma interval
+ax.fill_between(df_viz.x, df_viz.obs_lwr_68, df_viz.obs_upr_68, facecolor = 'crimson', alpha = 0.35)
+
+# include the randomly generated small data
+ax.scatter(df.x, df.y, color='k')
+
+
+# set labels
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+# show the plot
+plt.show()
+
+# %%
+df.y.std()
+# %%
+my_sigma
+# %%
+df['true_intercept'] = my_intercept
+df['true_slope'] = my_slope
+df['true_sigma'] = my_sigma
+
+#%%
+import os
+print("Current Working Directory:", os.getcwd())
+
+## Let's start setting file directory
+os.chdir(r"C:\Users\steve\OneDrive\桌面\PittMDS_DCC")
+print("New Working Directory:", os.getcwd())
+
+# %%
+df.to_csv('week_11_nonlinear_data.csv', index=False)
+# %%
